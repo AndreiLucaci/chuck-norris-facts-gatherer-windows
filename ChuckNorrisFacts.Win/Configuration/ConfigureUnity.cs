508f9b88105e2048083e32cdb93551718674f0d5
Andrei Lucaci <a.lucaci@netmatch.ro>
2017-07-14 16:49:16 +0300

    Fixed wrongful naming

--- ChuckNorrisFacts.Win/Configuration/ConfigureUnity.cs
+++ ChuckNorrisFacts.Win/Configuration/ConfigureUnity.cs
@@ -3,7 +3,7 @@ using System.Windows.Forms;
 using ChuckNorrisFacts.Win.Controllers;
 using Unity;
 
-namespace ChuckNorrisFacts.Win.Cofniguration
+namespace ChuckNorrisFacts.Win.Configuration
 {
 	public static class ConfigureUnity
 	{
--- ChuckNorrisFacts.Win/Program.cs
+++ ChuckNorrisFacts.Win/Program.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using System.Windows.Forms;
-using ChuckNorrisFacts.Win.Cofniguration;
+using ChuckNorrisFacts.Win.Configuration;
 using Unity;
 
 namespace ChuckNorrisFacts.Win
